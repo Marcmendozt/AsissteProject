5d8362192a4cc696586d1d1aa22d99aadb21137e
Marcos Julio Mendoza <mirio.marcos@gmail.com>
2023-07-30 13:24:49 -0500

    #001 ["Database model migration to MVC 5"]

---
+++ Asisste/AssistanceDialing.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistanceDialing.Entities
+{
+    public class Class1
+    {
+    }
+}
